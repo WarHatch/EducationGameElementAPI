b8094ce5b12a19128b7ba820b7eabb93c8955258
Karolis Staskevičius <staskeviciuskarolis@gmail.com>
2019-11-28 18:16:15 +0200

    CORS

--- src/server.ts
+++ src/server.ts
@@ -7,9 +7,13 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 app.use(mainRouter);
 
 const port = 8090;
 
-app.listen(port, () => console.info("Server running on port " + port));
+app.listen(port, () => console.log("Server running on port " + port));
