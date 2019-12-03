d0af38e75fff6dad2994b78bb01aea68490bfadc
Karolis Staskevičius <staskeviciuskarolis@gmail.com>
2019-12-03 18:35:43 +0200

    Example element with animation and onClick

--- src/routes/gameElements/dataSet.ts
+++ src/routes/gameElements/dataSet.ts
@@ -3,9 +3,15 @@ import { Router } from "express";
 const router = Router();
 
 router.get('/dataSet', (req, res) => {
-  const html = `<div>` + 
-    `<button onclick="console.log(\'I have been sent here to recognize CLICKS!\');" class="edugame-white s-4">OwO sum text</button>` +
-    `</div>`
+  const html = "<div>" + 
+      "<button id=\"slug\" " +
+        "onclick=" +
+          "\"console.log(\'I have been sent here to recognize CLICKS!\');" +
+          "document.getElementById('slug').remove();\" " +
+        "class=\"edugame-white s-4\">" +
+            "OwO sum text" +
+        "</button>" +
+    "</div>"
   const onClickFunc = () => {
     console.log("I have been sent here to recognize CLICKS!");
   };
