60b346eb0bb1e3dd00f61d6cf089ded69f7e1a46
Karolis Staskevičius <staskeviciuskarolis@gmail.com>
2019-12-10 18:17:04 +0200

    Rejected using `arrive` as a MutationObserver

--- src/ReactGenerators/public/SSRScript.ts
+++ src/ReactGenerators/public/SSRScript.ts
@@ -1,21 +1,20 @@
 console.log("server script received");
 
-// import arrive from "arrive";
+import arrive from "arrive";
 
 // CSS needs to be imported to be bundled
 import "./gameElementsStylesheet.css"
 import elementsFunctions from "../functions";
 
-const exampleButtonCallback = (data) => elementsFunctions.exampleButtonCallback(data);
-
-// Mount functions onto HTML (consider using `window`)
+// Mount functions onto HTML (consider using `window`?)
 // @ts-ignore
-document.exampleButtonCallback = exampleButtonCallback;
+document.exampleButtonCallback = elementsFunctions.exampleButtonCallback;
+// FIXME: Expose global functions?
+// https://webpack.js.org/guides/shimming/#exports-loader
 
-// Function mounting
 // document.arrive("[react-type='button']", function() {
 //   // 'this' refers to the newly created element
-//   this.onClick(() => exampleButtonCallback({"data":"mount-code server data"}))
+//   this.onClick(() => elementsFunctions.exampleButtonCallback({"data":"mount-code server data"}))
 // });
 
 console.log("server script finished");
