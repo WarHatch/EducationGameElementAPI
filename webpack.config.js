12de72580657e01b090eff9998acb322b82580d2
Karolis Staskevičius <staskeviciuskarolis@gmail.com>
2020-01-22 14:33:15 +0200

    attempt to define process.env

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      'process.env': JSON.stringify(process.env)
     })
   ],
   mode: 'development'
