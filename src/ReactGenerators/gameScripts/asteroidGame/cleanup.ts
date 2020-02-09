f93e672d3d508fabaf463a4b3b83421a4343dba8
Karolis Staskevičius <staskeviciuskarolis@gmail.com>
2020-02-09 15:13:03 +0200

    Fixed fall speed rounding

--- src/ReactGenerators/gameScripts/asteroidGame/cleanup.ts
+++ src/ReactGenerators/gameScripts/asteroidGame/cleanup.ts
@@ -1,7 +1,8 @@
 import { meteorContainerClassname } from "../../elements/meteorButton";
 
 const removeAllAsteroids = () => {
-  const endButtonCollection = document.body.getElementsByClassName(meteorContainerClassname);
+  // @ts-ignore gameElement is possibly 'null'
+  const endButtonCollection = document.querySelector("#game").getElementsByClassName(meteorContainerClassname);
   for (let i = 0; i < endButtonCollection.length; i++) {
     const endButton = endButtonCollection[i];
     endButton.remove();
