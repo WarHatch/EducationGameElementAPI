fd105e9f6d799c2e3a4a14caa4d4de000d63590f
Karolis Staskevičius <staskeviciuskarolis@gmail.com>
2020-02-25 17:13:28 +0200

    Fixed asteroid removal

--- src/ReactGenerators/gameScripts/asteroidGame/cleanup.ts
+++ src/ReactGenerators/gameScripts/asteroidGame/cleanup.ts
@@ -3,9 +3,8 @@ import { meteorContainerClassname } from "../../elements/meteorButton";
 const removeAllAsteroids = () => {
   // @ts-ignore gameElement is possibly 'null'
   const endButtonCollection = document.querySelector("#game").getElementsByClassName(meteorContainerClassname);
-  for (let i = 0; i < endButtonCollection.length; i++) {
-    const endButton = endButtonCollection[i];
-    endButton.remove();
+  for (let i = 0; i < endButtonCollection.length;) {
+    endButtonCollection.item(i)?.remove();
   }
 }
 
