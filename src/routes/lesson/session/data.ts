5d0e4ec2a6bb47383dd284cb7bf45caeb0922aa4
Karolis Staskevičius <staskeviciuskarolis@gmail.com>
2020-04-05 00:06:19 +0300

    Data analytics are no longer handled on the server

--- src/routes/lesson/session/data.ts
+++ src/routes/lesson/session/data.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import SeqDataModels from "../../../database/sequelize";
-import { countAverageReactionTime, countPercentCorrect } from "../../../routeDataParsing";
+// import { countAverageReactionTime, countPercentCorrect } from "../../../routeDataParsing";
 
 // Types
 import { ISession } from "../../../database/models/Session";
@@ -13,33 +13,35 @@ export type ISessionDataRequestModel = {
 const router = Router();
 
 router.post("/data", async (req, res) => {
-  const { body } = req;
+  // const { body } = req;
 
-  const { sessionId }: ISessionDataRequestModel = body;
-  try {
-    const dbData = await SeqDataModels.Session.findOne({
-      where: {
-        sessionId
-      },
+  // const { sessionId }: ISessionDataRequestModel = body;
+  // try {
+  //   const dbData = await SeqDataModels.Session.findOne({
+  //     where: {
+  //       sessionId
+  //     },
 
-      include: [
-        { model: SeqDataModels.AsteroidClickData }
-      ]
-    });
-    const { asteroidClickData }: ISession = dbData;
-    if (asteroidClickData === undefined) throw new Error("dbData.asteroidClickData is undefined");
-    const { correctPercentage, incorrectPercentage } = countPercentCorrect(asteroidClickData);
-    const responseBody = {
-      averageReactionTime: countAverageReactionTime(asteroidClickData),
-      correctPercentage,
-      fullData: dbData,
-      incorrectPercentage,
-    }
-    res.status(200).json(responseBody);
-  } catch (error) {
-    console.error(error);
-    res.status(400).send("Error while trying to fetch data");
-  }
+  //     include: [
+  //       { model: SeqDataModels.AsteroidClickData }
+  //     ]
+  //   });
+  //   const { asteroidClickData }: ISession = dbData;
+  //   if (asteroidClickData === undefined) throw new Error("dbData.asteroidClickData is undefined");
+  //   const { correctPercentage, incorrectPercentage } = countPercentCorrect(asteroidClickData);
+  //   const responseBody = {
+  //     averageReactionTime: countAverageReactionTime(asteroidClickData),
+  //     correctPercentage,
+  //     fullData: dbData,
+  //     incorrectPercentage,
+  //   }
+  //   res.status(200).json(responseBody);
+  // } catch (error) {
+  //   console.error(error);
+  //   res.status(400).send("Error while trying to fetch data");
+  // }
+  res.status(410).send("Data analytics are no longer handled on the server. " +
+    "averageReactionTime, correctPercentage should be counted on the client side")
 });
 
 export default router;
