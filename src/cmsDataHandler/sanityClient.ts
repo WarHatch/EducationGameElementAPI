07fd87d812d57664d993d3549a6d92d90f66a5b0
Karolis Staskevičius <staskeviciuskarolis@gmail.com>
2020-04-16 14:36:53 +0300

    expanded upon config setup

--- src/cmsDataHandler/sanityClient.ts
+++ src/cmsDataHandler/sanityClient.ts
@@ -1,8 +1,9 @@
 import sanityClient from "@sanity/client";
+import config from "../config";
 
 export default sanityClient({
   projectId: "0byunyul",
   dataset: "teachers_content",
-  token: process.env.SANITY_TOKEN, // leave blank to be anonymous
+  token: config.cmsConfig.SANITY_TOKEN, // leave blank to be anonymous
   useCdn: false // `false` if you want to ensure fresh data
 });
